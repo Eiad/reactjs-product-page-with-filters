4ca2a3ef9b24a6653fbec3013c4f0ff9532af9bc
Ash <eiad_work@yahoo.com>
2020-06-14 20:09:50 +0400

    Initial Upload
    Initial Upload - Main Files

---
+++ shirtsData.js
@@ -0,0 +1,61 @@
+export const products = [
+  {
+    id: 1,
+    productTitle: "Tiger Shirt - Red",
+    isActive: "red-border",
+    availSize: "S, M, L",
+    smallPicture: "https://i.imgur.com/Iyy2jVQ.jpg",
+    color: "red",
+    sleevType: "T-Shirt Y Collar",
+    brand: "Louis Vuitton",
+    price: "250",
+    currency: "AED",
+    descreption:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    thumb1: "https://i.imgur.com/6s7TCGu.jpg",
+    thumb2: "https://i.imgur.com/JZUsesm.jpg",
+    thumb3: "https://i.imgur.com/EBeCFad.jpg",
+    thumb4: "https://i.imgur.com/EBeCFad.jpg"
+  },
+
+  {
+    id: 2,
+    productTitle: "Tiger Shirt - Black",
+    isActive: "black-border",
+    availSize: "M, L, XL",
+    smallPicture: "https://i.imgur.com/QsZs2Uf.jpg",
+    color: "red",
+    sleevType: "T-Shirt Y Collar",
+    brand: "Louis Vuitton",
+    price: "300",
+    currency: "AED",
+    descreption:
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form",
+    thumb1: "https://i.imgur.com/H3zmz0C.jpg",
+    thumb2: "https://i.imgur.com/B0iZtjQ.jpg",
+    thumb3: "https://i.imgur.com/SrIXHYT.jpg",
+    thumb4: "https://i.imgur.com/iQQuXJV.jpg"
+  },
+  {
+    id: 3,
+    productTitle: "Tiger Shirt - Grey",
+    isActive: "grey-border",
+    availSize: "S, XL, XXL",
+    smallPicture: "https://i.imgur.com/gbQfOnC.jpg",
+    color: "red",
+    sleevType: "T-Shirt Y Collar",
+    brand: "Louis Vuitton",
+    price: "180",
+    currency: "AED",
+    descreption:
+      "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC.",
+    thumb1: "https://i.imgur.com/NcbLgoQ.jpg",
+    thumb2: "https://i.imgur.com/Mh9uVMb.jpg",
+    thumb3: "https://i.imgur.com/ZDYo15Z.jpg",
+    thumb4: "https://i.imgur.com/HB3ro5o.jpg"
+  }
+];
+
+export default {
+  products
+};
